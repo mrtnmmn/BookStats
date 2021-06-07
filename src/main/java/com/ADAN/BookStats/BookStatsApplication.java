2dc9436b8fe9f92f52f142358b179d8c74fcdc18
mrtnmmn <mrtn.lmba@gmail.com>
2021-06-07 18:30:14 +0200

    F>irst commit

---
+++ src/main/java/com/ADAN/BookStats/BookStatsApplication.java
@@ -0,0 +1,13 @@
+package com.ADAN.BookStats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStatsApplication.class, args);
+	}
+
+}
